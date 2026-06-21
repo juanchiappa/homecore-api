0ade4444e6dd09f62faa815fea252be26b1b563a
Juani <chiappajuanignacio@gmail.com>
2026-06-21 13:44:59 -0300

    Extend ServiceMonitorConfig and add ServiceStatus The ServiceMonitorConfig class has been made public, and new properties have been added to it to describe monitored services. In addition, the ServiceStatus public class has been created to represent the health status of services.

--- HomeCore.Entities/ServiceMonitorConfig.cs
+++ HomeCore.Entities/ServiceMonitorConfig.cs
@@ -4,7 +4,18 @@ using System.Text;
 
 namespace HomeCore.Entities
 {
-    internal class ServiceMonitorConfig
+    public class ServiceMonitorConfig
     {
+        public string Name { get; set; } = string.Empty;
+        public string Type { get; set; } = string.Empty; // "http_healthcheck" | "docker_container"
+        public string? Url { get; set; }
+        public string? ContainerName { get; set; }
+    }
+    public class ServiceStatus
+    {
+        public string Name { get; set; } = string.Empty;
+        public bool IsHealthy { get; set; }
+        public string? Detail { get; set; }
+        public DateTime CheckedAt { get; set; } = DateTime.UtcNow;
     }
 }
