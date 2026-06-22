ddf8ea8e5932746f7e8525d2322af0028eea591a
Juani <chiappajuanignacio@gmail.com>
2026-06-21 23:10:49 -0300

    Add login DTOs and references to DAL and JWT packages The LoginRequestDto and LoginResponseDto classes are added in HomeCore.Entities.DTOs to handle authentication. Added reference to HomeCore.DAL in HomeCore.API and new identity NuGet and JWT packages in HomeCore.BLL.

---
+++ HomeCore.Entities/DTOs/LoginRequestDto.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace HomeCore.Entities.DTOs
+{
+    public class LoginRequestDto
+    {
+        [Required]
+        public string Username { get; set; } = string.Empty;
+
+        [Required]
+        public string Passwword { get; set; } = string.Empty;
+
+    }
+}
---
+++ HomeCore.Entities/DTOs/LoginResponseDto.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HomeCore.Entities.DTOs
+{
+    public class LoginResponseDto
+    {
+        public string Token { get; set; } = string.Empty;
+        public DateTime ExpiresAt { get; set; }
+    }
+}
