74793ca1cc02d9f2d8874d101d3eff45a8d8a505
Juani <chiappajuanignacio@gmail.com>
2026-07-04 21:16:18 -0300

    Adds support for Docker and container entities Docker.DotNet is added to the BLL project to interact with Docker. The ContainerInfo and ContainerStats classes are created in HomeCore.Entities to represent Docker container information and statistics.

---
+++ HomeCore.Entities/ContainerInfo.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HomeCore.Entities
+{
+    public class ContainerInfo
+    {
+        public string Id { get; set; } = string.Empty;
+        public string ShortId => Id.Length >= 12 ? Id[..12] : Id;
+        public string Name { get; set; } = string.Empty;
+        public string Image { get; set; } = string.Empty;
+        public string State { get; set; } = string.Empty;   // "running", "exited", …
+        public string Status { get; set; } = string.Empty;  // "Up 2 hours", "Exited (0) 3 days ago"
+        public DateTime Created { get; set; }
+    }
+}
---
+++ HomeCore.Entities/ContainerStats.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HomeCore.Entities
+{
+    public class ContainerStats
+    {
+        public string ContainerId { get; set; } = string.Empty;
+        public string ContainerName { get; set; } = string.Empty;
+        public double CpuPercent { get; set; }
+        public long MemoryUsageBytes { get; set; }
+        public long MemoryLimitBytes { get; set; }
+        public double MemoryPercent { get; set; }
+        public long NetworkRxBytes { get; set; }
+        public long NetworkTxBytes { get; set; }
+        public DateTime ReadAt { get; set; }
+    }
+}
