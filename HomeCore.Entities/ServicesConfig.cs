34a742f748a2c2f353927c55e4b10871e865b606
Juani <chiappajuanignacio@gmail.com>
2026-07-06 21:42:03 -0300

    Add ServicesConfig class for service configuration The ServicesConfig.cs file is created with the ServicesConfig class in the HomeCore.Entities namespace. It includes a Services property that is a list of ServiceMonitorConfig, initialized by default, to manage the configuration of monitored services.

---
+++ HomeCore.Entities/ServicesConfig.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HomeCore.Entities
+{
+    public class ServicesConfig
+    {
+        public List<ServiceMonitorConfig> Services { get; set; } = new();
+    }
+}
